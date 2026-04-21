f672426a498a38efe0e15224934317902308add1
tinchoweinbaum <martinweinbaum123@gmail.com>
2026-04-21 13:44:42 -0300

    import scheduler

--- ui/app.py
+++ ui/app.py
@@ -1,12 +1,23 @@
+import importlib.util
 import sys
 import threading
 
 from flask import Flask, render_template
 from pathlib import Path
+from pathlib import Path
 
+# scheduler.py se importa dandole el path absoluto xq python es una pija.
 BASE_DIR = Path(__file__).resolve().parent
-sys.path.append(str(BASE_DIR.parent))
+path_al_scheduler = BASE_DIR.parent / "scheduler.py"
+
+spec = importlib.util.spec_from_file_location("scheduler", str(path_al_scheduler))
+scheduler_module = importlib.util.module_from_spec(spec)
+sys.modules["scheduler"] = scheduler_module
+spec.loader.exec_module(scheduler_module)
+
 from scheduler import Scheduler
+
+
 
 "Flask lo que hace es linkear funciones usando decoradores, con direcciones de la página web, entonces /app/reiniciar llama a la función decorada con @app.route('/reiniciar')"
 
